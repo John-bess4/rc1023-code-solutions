cf64aefb573c15c621f57752ec974df7c669b6c2
John-bess4 <jbesss247@gmail.com>
2023-10-23 18:44:48 +0000

    javascript-primitives-and-variables completed

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,17 @@
+const fullName = 'Cody Miller';
+const isCool = true;
+const totalPets = 1000;
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+
+const unknown = null;
+
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+let never;
+
+console.log('value of never:', never);
+console.log('typeof never', never);
