2a4321c87568ddccf34245346d3f4d21a35c2bc9
John-bess4 <jbesss247@gmail.com>
2023-12-07 00:34:33 +0000

    ts2

---
+++ typescript-primitives-and-variables/main.ts
@@ -0,0 +1,21 @@
+const fullName: string = 'John Bessemer';
+const isCool: boolean = true;
+const totalPets: number = 2;
+
+console.log("Full Name:", fullName);
+console.log("Is Cool:", isCool);
+console.log("Total Pets:", totalPets);
+
+console.log('Type Of Full Name:', typeof fullName);
+console.log('Type Of Is Cool:', typeof isCool);
+console.log('Type Of Total Pets:', typeof totalPets);
+
+const unknown: null = null;
+
+console.log("Unknown:", unknown);
+console.log('Type Of Unknown:', typeof unknown);
+
+let never: undefined;
+
+console.log('Never:', never);
+console.log('Type Of Never:', typeof never);
